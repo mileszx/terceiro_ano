91e0403d5b1536c2429af50eeb8d66fd54dbc131
mileszx <milesmoraleszx26@gmail.com>
2026-02-24 14:14:04 -0300

    .

---
+++ aulas_php_mySQL/index.php/index.php
@@ -0,0 +1,13 @@
+<?php
+// comandos de saída 
+//echo
+echo "<h1>Olá Mundo!</h1>";
+
+//print
+$mensagem = "Olá Mundo!";
+
+$nome = "victor";
+$idade = 17;
+$altura = 1.80;
+$endereço = "Grand Line, 123";
+echo "nome: $nome, idade: $idade altura: $altura metros, enedereço: $endereço";
